49a51e3676ebe71d3e83565490b9e068e1bfce79
Semyon1104 <129722895+Semyon1104@users.noreply.github.com>
2024-05-08 09:56:36 +0300

    Semyon1104/binary heap (#5)
    * add apps and binheap
    
    * add test
    
    * clang

---
+++ app/HListApp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "HierarchicalList.h"
+
+using namespace std;
+using namespace HListLib;
+
+int main() {
+  HList<int> list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  list.pushBack(4);
+  list.pushBack(5);
+
+  HList<int> sublist;
+  sublist.pushBack(6);
+  sublist.pushBack(7);
+  sublist.pushBack(8);
+  list.addSublistToElementWithValue(3, sublist);
+
+  cout << "Main List:" << endl;
+  list.traverse(list.getRoot());
+
+  hlistNode<int>* nodeToRemove = list.findElement(list, 2);
+  list.remove(nodeToRemove);
+
+  cout << "\nMain List after removing 2 elem:" << endl;
+  list.traverse(list.getRoot());
+
+  int searchValue = 4;
+  hlistNode<int>* foundNode = list.findElement(list, searchValue);
+  if (foundNode) {
+    cout << "\nelem is found -  " << searchValue << endl;
+  } else {
+    cout << "\nelem " << searchValue << " not found" << endl;
+  }
+
+  list.clear();
+  cout << "\nmain list after cleaning:" << endl;
+  list.traverse(list.getRoot());
+
+  return 0;
+}
---
+++ app/ListApp.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "TList.h"
+
+using namespace std;
+using namespace ListLib;
+
+int main() {
+  SkipList<int> skipList(4);
+
+  skipList.insert(10);
+  skipList.insert(20);
+  skipList.insert(30);
+  skipList.insert(40);
+  skipList.insert(50);
+
+  cout << "SkipList after adding elems: ";
+  skipList.print();
+
+  int searchValue = 30;
+  cout << "find elem " << searchValue << " ...." << endl;
+  if (skipList.find(searchValue)) {
+    cout << "elem " << searchValue << " is found in skiplist." << endl;
+  } else {
+    cout << "elem " << searchValue << " is not found in skiplist." << endl;
+  }
+
+  searchValue = 15;
+  cout << "find elem " << searchValue << " ...." << endl;
+  if (skipList.find(searchValue)) {
+    cout << "elem " << searchValue << " is found in skiplist." << endl;
+  } else {
+    cout << "elem " << searchValue << " is not found in skiplist." << endl;
+  }
+
+  skipList.remove(30);
+
+  cout << "SkipList after removing elem 30: ";
+  skipList.print();
+
+  return 0;
+}
